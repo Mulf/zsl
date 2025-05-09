1dd3c9a440354a57a54dc3cf4cb0f69053a9373f
mulf <1145173206@qq.com>
2025-05-10 02:52:03 +0800

    Exponents and Logarithms

--- zsl/include/zatlab/mathematics/elementary_math.h
+++ zsl/include/zatlab/mathematics/elementary_math.h
@@ -7,45 +7,81 @@ namespace zsl {
 #pragma region Trigonometry
 // sin
 vector_d sin(const vector_d &v);
+vector_c sin(const vector_c &v);
 matrix_d sin(const matrix_d &v);
+matrix_c sin(const matrix_c &A);
 vector_d sind(const vector_d &v);
+vector_c sind(const vector_c &v);
 matrix_d sind(const matrix_d &A);
+matrix_c sind(const matrix_c &A);
 vector_d sinpi(const vector_d &v);
+vector_c sinpi(const vector_c &v);
 matrix_d sinpi(const matrix_d &A);
+matrix_c sinpi(const matrix_c &A);
 vector_d asin(const vector_d &v);
+vector_c asin(const vector_c &v);
 matrix_d asin(const matrix_d &A);
+matrix_c asin(const matrix_c &A);
 vector_d sinh(const vector_d &v);
+vector_c sinh(const vector_c &v);
 matrix_d sinh(const matrix_d &A);
+matrix_c sinh(const matrix_c &A);
 vector_d asinh(const vector_d &v);
+vector_c asinh(const vector_c &v);
 matrix_d asinh(const matrix_d &A);
+matrix_c asinh(const matrix_c &A);
 
 // cos
 vector_d cos(const vector_d &v);
+vector_c cos(const vector_c &v);
 matrix_d cos(const matrix_d &v);
+matrix_c cos(const matrix_c &A);
 vector_d cosd(const vector_d &v);
+vector_c cosd(const vector_c &v);
 matrix_d cosd(const matrix_d &A);
+matrix_c cosd(const matrix_c &A);
 vector_d cospi(const vector_d &v);
+vector_c cospi(const vector_c &v);
 matrix_d cospi(const matrix_d &A);
+matrix_c cospi(const matrix_c &A);
 vector_d acos(const vector_d &v);
+vector_c acos(const vector_c &v);
 matrix_d acos(const matrix_d &A);
+matrix_c acos(const matrix_c &A);
 vector_d cosh(const vector_d &v);
+vector_c cosh(const vector_c &v);
 matrix_d cosh(const matrix_d &A);
+matrix_c cosh(const matrix_c &A);
 vector_d acosh(const vector_d &v);
+vector_c acosh(const vector_c &v);
 matrix_d acosh(const matrix_d &A);
+matrix_c acosh(const matrix_c &A);
 
 // tan
 vector_d tan(const vector_d &v);
+vector_c tan(const vector_c &v);
 matrix_d tan(const matrix_d &v);
+matrix_c tan(const matrix_c &A);
 vector_d tand(const vector_d &v);
+vector_c tand(const vector_c &v);
 matrix_d tand(const matrix_d &A);
+matrix_c tand(const matrix_c &A);
 vector_d tanpi(const vector_d &v);
+vector_c tanpi(const vector_c &v);
 matrix_d tanpi(const matrix_d &A);
+matrix_c tanpi(const matrix_c &A);
 vector_d atan(const vector_d &v);
+vector_c atan(const vector_c &v);
 matrix_d atan(const matrix_d &A);
+matrix_c atan(const matrix_c &A);
 vector_d tanh(const vector_d &v);
+vector_c tanh(const vector_c &v);
 matrix_d tanh(const matrix_d &A);
+matrix_c tanh(const matrix_c &A);
 vector_d atanh(const vector_d &v);
+vector_c atanh(const vector_c &v);
 matrix_d atanh(const matrix_d &A);
+matrix_c atanh(const matrix_c &A);
 
 // csc
 vector_d csc(const vector_d &v);
@@ -75,7 +111,6 @@ matrix_d sech(const matrix_d &A);
 vector_d asech(const vector_d &v);
 matrix_d asech(const matrix_d &A);
 
-
 // cot
 vector_d cot(const vector_d &v);
 matrix_d cot(const matrix_d &v);
@@ -97,6 +132,42 @@ matrix_d deg2rad(const matrix_d &A);
 double rad2deg(double x);
 vector_d rad2deg(const vector_d &v);
 matrix_d rad2deg(const matrix_d &A);
+
+#pragma endregion
+
+#pragma region Exponents and Logarithms
+//exp
+vector_d exp(const vector_d &v);
+vector_c exp(const vector_c &v);
+matrix_d exp(const matrix_d &A);
+matrix_c exp(const matrix_c &A);
+// log
+vector_d log(const vector_d &v);
+vector_c log(const vector_c &v);
+matrix_d log(const matrix_d &A);
+matrix_c log(const matrix_c &A);
+// log10
+vector_d log10(const vector_d &v);
+vector_c log10(const vector_c &v);
+matrix_d log10(const matrix_d &A);
+matrix_c log10(const matrix_c &A);
+// log2
+vector_d log2(const vector_d &v);
+vector_c log2(const vector_c &v);
+matrix_d log2(const matrix_d &A);
+matrix_c log2(const matrix_c &A);
+// pow2
+double pow2(double x);
+complex_d pow2(const complex_d &z);
+vector_d pow2(const vector_d &v);
+vector_c pow2(const vector_c &v);
+matrix_d pow2(const matrix_d &A);
+matrix_c pow2(const matrix_c &A);
+// sqrt
+vector_d sqrt(const vector_d &v);
+vector_c sqrt(const vector_c &v);
+matrix_d sqrt(const matrix_d &A);
+matrix_c sqrt(const matrix_c &A);
 
 #pragma endregion
 }
--- zsl/src/zatlab/mathematics/elementary_math.cpp
+++ zsl/src/zatlab/mathematics/elementary_math.cpp
@@ -2,14 +2,21 @@
 #include <cmath>
 #include <algorithm>
 
-#define DEG_TO_RAD(x)  (x * zsl::pi / 180)
-#define RAD_TO_DEG(x)	(x * 180 / zsl::pi)
+#define DEG_TO_RAD(x)  (x * zsl::pi / 180.0)
+#define RAD_TO_DEG(x)	(x * 180.0 / zsl::pi)
+
+
+
 
 namespace _ {
 using namespace zsl;
 
-double deg2rad(double d) {
-	return d * pi / 180;
+static inline double pow2(double x) {
+	return std::pow(2, x);
+}
+
+static inline complex_d pow2(const complex_d &z) {
+	return std::pow(2, z);
 }
 
 vector_d unary_func(const vector_d &x, double (*func)(double)) {
@@ -20,14 +27,13 @@ vector_d unary_func(const vector_d &x, double (*func)(double)) {
 	return y;
 }
 
-//vector_d unary_func(const vector_d &x, const UnaryOp &func) {
-//	vector_d y(x.size());
-//	std::transform(x.begin(), x.end(), y.begin(), [&](double x){
-//		return func(x);
-//	});
-//	return y;
-//}
-
+vector_c unary_func(const vector_c &x, complex_d (*func)(const complex_d &)) {
+	vector_c y(x.size());
+	std::transform(x.begin(), x.end(), y.begin(), [&](const complex_d &x){
+		return func(x);
+	});
+	return y;
+}
 
 matrix_d unary_func(const matrix_d &X, double (*func)(double)) {
 	matrix_d Y(X.size());
@@ -37,13 +43,13 @@ matrix_d unary_func(const matrix_d &X, double (*func)(double)) {
 	return Y;
 }
 
-//matrix_d unary_func(const matrix_d &X, const UnaryOp &func) {
-//	matrix_d Y(X.size());
-//	std::transform(X.begin(), X.end(), Y.begin(), [&](const vector_d &x){
-//		return unary_func(x, func);
-//	});
-//	return Y;
-//}
+matrix_c unary_func(const matrix_c &X, complex_d (*func)(const complex_d &)) {
+	matrix_c Y(X.size());
+	std::transform(X.begin(), X.end(), Y.begin(), [&](const vector_c &x){
+		return unary_func(x, func);
+	});
+	return Y;
+}
 
 }
 
@@ -54,12 +60,26 @@ vector_d sin(const vector_d &v) {
 	return _::unary_func(v, std::sin);
 }
 
+vector_c sin(const vector_c &v) {
+	return _::unary_func(v, std::sin);
+}
+
 matrix_d sin(const matrix_d &A) {
+	return _::unary_func(A, std::sin);
+}
+
+matrix_c sin(const matrix_c &A) {
 	return _::unary_func(A, std::sin);
 }
 
 vector_d sind(const vector_d &v) {
 	return _::unary_func(v, +[](double x) {
+		return std::sin(DEG_TO_RAD(x));
+	});
+}
+
+vector_c sind(const vector_c &v) {
+	return _::unary_func(v, +[](const complex_d &x) {
 		return std::sin(DEG_TO_RAD(x));
 	});
 }
@@ -70,8 +90,20 @@ matrix_d sind(const matrix_d &A) {
 	});
 }
 
+matrix_c sind(const matrix_c &A) {
+	return _::unary_func(A, +[](const complex_d &x) {
+		return std::sin(DEG_TO_RAD(x));
+	});
+}
+
 vector_d sinpi(const vector_d &v) {
 	return _::unary_func(v, +[](double x) -> double{
+		return std::sin(x * pi);
+	});
+}
+
+vector_c sinpi(const vector_c &v) {
+	return _::unary_func(v, +[](const complex_d &x) {
 		return std::sin(x * pi);
 	});
 }
@@ -82,7 +114,17 @@ matrix_d sinpi(const matrix_d &A) {
 	});
 }
 
+matrix_c sinpi(const matrix_c &A) {
+	return _::unary_func(A, +[](const complex_d &x){
+		return std::sin(x * pi);
+	});
+}
+
 vector_d asin(const vector_d &v) {
+	return _::unary_func(v, std::asin);
+}
+
+vector_c asin(const vector_c &v) {
 	return _::unary_func(v, std::asin);
 }
 
@@ -90,7 +132,15 @@ matrix_d asin(const matrix_d &A) {
 	return _::unary_func(A, std::asin);
 }
 
+matrix_c asin(const matrix_c &A) {
+	return _::unary_func(A, std::asin);
+}
+
 vector_d sinh(const vector_d &v) {
+	return _::unary_func(v, std::sinh);
+}
+
+vector_c sinh(const vector_c &v) {
 	return _::unary_func(v, std::sinh);
 }
 
@@ -98,11 +148,24 @@ matrix_d sinh(const matrix_d &A) {
 	return _::unary_func(A, std::sinh);
 }
 
+matrix_c sinh(const matrix_c &A) {
+	return _::unary_func(A, std::sinh);
+}
+
 vector_d asinh(const vector_d &v) {
 	return _::unary_func(v, std::asinh);
 }
 
+vector_c asinh(const vector_c &A) {
+	return _::unary_func(A, std::asinh);
+}
+
+
 matrix_d asinh(const matrix_d &A) {
+	return _::unary_func(A, std::asinh);
+}
+
+matrix_c asinh(const matrix_c &A) {
 	return _::unary_func(A, std::asinh);
 }
 
@@ -111,12 +174,26 @@ vector_d cos(const vector_d &v) {
 	return _::unary_func(v, std::cos);
 }
 
+vector_c cos(const vector_c &v) {
+	return _::unary_func(v, std::cos);
+}
+
 matrix_d cos(const matrix_d &A) {
+	return _::unary_func(A, std::cos);
+}
+
+matrix_c cos(const matrix_c &A) {
 	return _::unary_func(A, std::cos);
 }
 
 vector_d cosd(const vector_d &v) {
 	return _::unary_func(v, +[](double x) {
+		return std::cos(DEG_TO_RAD(x));
+	});
+}
+
+vector_c cosd(const vector_c &v) {
+	return _::unary_func(v, +[](const complex_d &x) {
 		return std::cos(DEG_TO_RAD(x));
 	});
 }
@@ -127,8 +204,20 @@ matrix_d cosd(const matrix_d &A) {
 	});
 }
 
+matrix_c cosd(const matrix_c &A) {
+	return _::unary_func(A, +[](const complex_d &x) {
+		return std::cos(DEG_TO_RAD(x));
+	});
+}
+
 vector_d cospi(const vector_d &v) {
 	return _::unary_func(v, +[](double x) -> double{
+		return std::cos(x * pi);
+	});
+}
+
+vector_c cospi(const vector_c &v) {
+	return _::unary_func(v, +[](const complex_d &x) {
 		return std::cos(x * pi);
 	});
 }
@@ -139,7 +228,17 @@ matrix_d cospi(const matrix_d &A) {
 	});
 }
 
+matrix_c cospi(const matrix_c &A) {
+	return _::unary_func(A, +[](const complex_d &x){
+		return std::cos(x * pi);
+	});
+}
+
 vector_d acos(const vector_d &v) {
+	return _::unary_func(v, std::acos);
+}
+
+vector_c acos(const vector_c &v) {
 	return _::unary_func(v, std::acos);
 }
 
@@ -147,7 +246,15 @@ matrix_d acos(const matrix_d &A) {
 	return _::unary_func(A, std::acos);
 }
 
+matrix_c acos(const matrix_c &A) {
+	return _::unary_func(A, std::acos);
+}
+
 vector_d cosh(const vector_d &v) {
+	return _::unary_func(v, std::cosh);
+}
+
+vector_c cosh(const vector_c &v) {
 	return _::unary_func(v, std::cosh);
 }
 
@@ -155,11 +262,23 @@ matrix_d cosh(const matrix_d &A) {
 	return _::unary_func(A, std::cosh);
 }
 
+matrix_c cosh(const matrix_c &A) {
+	return _::unary_func(A, std::cosh);
+}
+
 vector_d acosh(const vector_d &v) {
 	return _::unary_func(v, std::acosh);
 }
 
+vector_c acosh(const vector_c &A) {
+	return _::unary_func(A, std::acosh);
+}
+
 matrix_d acosh(const matrix_d &A) {
+	return _::unary_func(A, std::acosh);
+}
+
+matrix_c acosh(const matrix_c &A) {
 	return _::unary_func(A, std::acosh);
 }
 
@@ -168,12 +287,26 @@ vector_d tan(const vector_d &v) {
 	return _::unary_func(v, std::tan);
 }
 
+vector_c tan(const vector_c &v) {
+	return _::unary_func(v, std::tan);
+}
+
 matrix_d tan(const matrix_d &A) {
+	return _::unary_func(A, std::tan);
+}
+
+matrix_c tan(const matrix_c &A) {
 	return _::unary_func(A, std::tan);
 }
 
 vector_d tand(const vector_d &v) {
 	return _::unary_func(v, +[](double x) {
+		return std::tan(DEG_TO_RAD(x));
+	});
+}
+
+vector_c tand(const vector_c &v) {
+	return _::unary_func(v, +[](const complex_d &x) {
 		return std::tan(DEG_TO_RAD(x));
 	});
 }
@@ -184,8 +317,20 @@ matrix_d tand(const matrix_d &A) {
 	});
 }
 
+matrix_c tand(const matrix_c &A) {
+	return _::unary_func(A, +[](const complex_d &x) {
+		return std::tan(DEG_TO_RAD(x));
+	});
+}
+
 vector_d tanpi(const vector_d &v) {
 	return _::unary_func(v, +[](double x) -> double{
+		return std::tan(x * pi);
+	});
+}
+
+vector_c tanpi(const vector_c &v) {
+	return _::unary_func(v, +[](const complex_d &x) {
 		return std::tan(x * pi);
 	});
 }
@@ -196,7 +341,17 @@ matrix_d tanpi(const matrix_d &A) {
 	});
 }
 
+matrix_c tanpi(const matrix_c &A) {
+	return _::unary_func(A, +[](const complex_d &x){
+		return std::tan(x * pi);
+	});
+}
+
 vector_d atan(const vector_d &v) {
+	return _::unary_func(v, std::atan);
+}
+
+vector_c atan(const vector_c &v) {
 	return _::unary_func(v, std::atan);
 }
 
@@ -204,7 +359,15 @@ matrix_d atan(const matrix_d &A) {
 	return _::unary_func(A, std::atan);
 }
 
+matrix_c atan(const matrix_c &A) {
+	return _::unary_func(A, std::atan);
+}
+
 vector_d tanh(const vector_d &v) {
+	return _::unary_func(v, std::tanh);
+}
+
+vector_c tanh(const vector_c &v) {
 	return _::unary_func(v, std::tanh);
 }
 
@@ -212,11 +375,24 @@ matrix_d tanh(const matrix_d &A) {
 	return _::unary_func(A, std::tanh);
 }
 
+matrix_c tanh(const matrix_c &A) {
+	return _::unary_func(A, std::tanh);
+}
+
 vector_d atanh(const vector_d &v) {
 	return _::unary_func(v, std::atanh);
 }
 
+vector_c atanh(const vector_c &A) {
+	return _::unary_func(A, std::atanh);
+}
+
+
 matrix_d atanh(const matrix_d &A) {
+	return _::unary_func(A, std::atanh);
+}
+
+matrix_c atanh(const matrix_c &A) {
 	return _::unary_func(A, std::atanh);
 }
 
@@ -316,7 +492,7 @@ vector_d acsch(const vector_d &v) {
 	}
 
 	return _::unary_func(v, +[](double x){
-		return std::log(1 / x + sqrt(1 / (x * x) + 1));
+		return std::log(1 / x + std::sqrt(1 / (x * x) + 1));
 	});
 }
 
@@ -330,7 +506,7 @@ matrix_d acsch(const matrix_d &A) {
 	}
 
 	return _::unary_func(A, +[](double x){
-		return std::log(1 / x + sqrt(1 / (x * x) + 1));
+		return std::log(1 / x + std::sqrt(1 / (x * x) + 1));
 	});
 }
 
@@ -429,7 +605,7 @@ vector_d asech(const vector_d &v) {
 	}
 
 	return _::unary_func(v, +[](double x){
-		return std::log(1 / x + sqrt(1 / (x * x) + 1));
+		return std::log(1 / x + std::sqrt(1 / (x * x) + 1));
 	});
 }
 
@@ -443,7 +619,7 @@ matrix_d asech(const matrix_d &A) {
 	}
 
 	return _::unary_func(A, +[](double x){
-		return std::log(1 / x + sqrt(1 / (x * x) - 1));
+		return std::log(1 / x + std::sqrt(1 / (x * x) - 1));
 	});
 }
 
@@ -605,6 +781,121 @@ matrix_d rad2deg(const matrix_d &A) {
 	return B;
 }
 
+#pragma endregion
 
+#pragma region  Exponents and Logarithms
+// exp
+vector_d exp(const vector_d &v) {
+	return _::unary_func(v, std::exp);
+}
+
+vector_c exp(const vector_c &v) {
+	return _::unary_func(v, std::exp);
+}
+
+matrix_d exp(const matrix_d &A) {
+	return _::unary_func(A, std::exp);
+}
+
+matrix_c exp(const matrix_c &A) {
+	return _::unary_func(A, std::exp);
+}
+
+// log
+vector_d log(const vector_d &v) {
+	return _::unary_func(v, std::log);
+}
+
+vector_c log(const vector_c &v) {
+	return _::unary_func(v, std::log);
+}
+
+matrix_d log(const matrix_d &A) {
+	return _::unary_func(A, std::log);
+}
+
+matrix_c log(const matrix_c &A) {
+	return _::unary_func(A, std::log);
+}
+
+// log10
+vector_d log10(const vector_d &v) {
+	return _::unary_func(v, std::log10);
+}
+
+vector_c log10(const vector_c &v) {
+	return _::unary_func(v, std::log10);
+}
+
+matrix_d log10(const matrix_d &A) {
+	return _::unary_func(A, std::log10);
+}
+
+matrix_c log10(const matrix_c &A) {
+	return _::unary_func(A, std::log10);
+}
+
+// log2
+vector_d log2(const vector_d &v) {
+	return _::unary_func(v, std::log2);
+}
+
+vector_c log2(const vector_c &v) {
+	return _::unary_func(v, +[](const complex_d &z){
+		return complex_d{std::log2(std::abs(z)), std::arg(z) / std::log(2)};
+	});
+}
+
+matrix_d log2(const matrix_d &A) {
+	return _::unary_func(A, std::log2);
+}
+
+matrix_c log2(const matrix_c &A) {
+	return _::unary_func(A, +[](const complex_d &z){
+		return complex_d{std::log2(std::abs(z)), std::arg(z) / std::log(2)};
+	});
+}
+
+// pow2
+double pow2(double x) {
+	return _::pow2(x);
+}
+
+complex_d pow2(const complex_d &z) {
+	return _::pow2(z);
+}
+
+vector_d pow2(const vector_d &v) {
+	return _::unary_func(v, _::pow2);
+}
+
+vector_c pow2(const vector_c &v) {
+	return _::unary_func(v, _::pow2);
+}
+
+matrix_d pow2(const matrix_d &A) {
+	return _::unary_func(A, _::pow2);
+}
+
+matrix_c pow2(const matrix_c &A) {
+	return _::unary_func(A, _::pow2);
+}
+
+// sqrt
+vector_d sqrt(const vector_d &v) {
+	return _::unary_func(v, std::sqrt);
+}
+
+vector_c sqrt(const vector_c &v) {
+	return _::unary_func(v, std::sqrt);
+}
+
+matrix_d sqrt(const matrix_d &A) {
+	return _::unary_func(A, std::sqrt);
+}
+
+matrix_c sqrt(const matrix_c &A) {
+	return _::unary_func(A, std::sqrt);
+}
 #pragma endregion
 }
